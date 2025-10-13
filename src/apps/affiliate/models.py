47d3837a85e34d5d8462aeffca06e5bdcf31889a
Alixandria007 <juninhokrauts778@gmail.com>
2025-10-12 23:01:20 -0300

    Inicio do model de Affiliate.

--- src/apps/affiliate/models.py
+++ src/apps/affiliate/models.py
@@ -1,11 +1,28 @@
 from django.db import models
 
+from apps.user.models import User
+
+
 class Affiliate(models.Model):
     class Meta:
         verbose_name: str = "Affiliate"
         verbose_name_plural: str = "Affiliates"
 
-    ...
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    code = models.CharField(max_length=26)
+    commission_balance = models.DecimalField()
+    total_earned = models.DecimalField()
 
     def __str__(self) -> str:
-        return f"Afiliado {...}"
+        return f"Afiliado {self.user.username}"
+
+
+class Sale(models.Model):
+    class Meta:
+        verbose_name: str = "Affiliate"
+        verbose_name_plural: str = "Affiliates"
+
+    affiliate = models.ForeignKey(Affiliate, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self) -> str:
+        return f"Venda Nº{self.pk}"
--- src/apps/customer/models.py
+++ src/apps/customer/models.py
@@ -1,11 +1,25 @@
 from django.db import models
 
+from apps.user.models import User
+
+
 class Customer(models.Model):
     class Meta:
         verbose_name: str = "Customer"
         verbose_name_plural: str = "Customers"
 
-    ...
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return f"Cliente {...}"
+
+
+class Subscription(models.Model):
+    class Meta:
+        verbose_name: str = "Subscription"
+        verbose_name_plural: str = "Subscriptions"
+
+    costumer = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"Assinatura {...}"
