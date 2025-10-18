478ce9415aeb86219750f9c8f70e042cddabf104
Alixandria007 <juninhokrauts778@gmail.com>
2025-10-17 23:30:46 -0300

    Views de Auth prontas.

--- src/apps/user/views.py
+++ src/apps/user/views.py
@@ -1,6 +1,9 @@
-from rest_framework import generics, status
+from backend.src.apps.user.serializer import LoginUserSerializer
+from rest_framework import generics, permissions, status
 from rest_framework.request import Request
 from rest_framework.response import Response
+from rest_framework_simplejwt.exceptions import TokenError
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from utils.base_view import BaseUserView
 
@@ -39,3 +42,67 @@ class UserCreateView(BaseUserView, generics.CreateAPIView):
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         return Response("Usuario criado com sucesso.", status=status.HTTP_201_CREATED)
+
+
+class LoginView(generics.CreateAPIView):
+    serializer_class = LoginUserSerializer
+    permission_classes = (permissions.AllowAny,)
+
+    def create(
+        self, request: Request, *arg: object, **kwargs: dict[object, object]
+    ) -> Response:
+        serializer = self.get_serializer(data=request.data)
+        print(serializer.is_valid())
+        serializer.is_valid(raise_exception=True)
+
+        user = serializer.validated_data["user"]
+        refresh = RefreshToken.for_user(user)
+
+        return Response(
+            {
+                "access": str(refresh.access_token),
+                "refresh": str(refresh),
+            },
+            status=status.HTTP_200_OK,
+        )
+
+
+class RefreshView(generics.GenericAPIView):
+    permission_classes = (permissions.AllowAny,)
+
+    def post(
+        self, request: Request, *args: object, **kwargs: dict[object, object]
+    ) -> Response:
+        refresh_token = request.data.get("refresh")
+
+        if not refresh_token:
+            return Response(
+                {"detail": "Token de atualização não fornecido."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        try:
+            refresh = RefreshToken(refresh_token)
+            access_token = str(refresh.access_token)
+
+            return Response(
+                {"access": access_token, "detail": "Token atualizado com sucesso."},
+                status=status.HTTP_200_OK,
+            )
+
+        except TokenError as e:
+            return Response(
+                {"detail": f"Token inválido: {e!s}"},
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
+
+
+class LogoutView(generics.CreateAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def post(
+        self, request: Request, *args: object, **kwargs: dict[object, object]
+    ) -> Response:
+        return Response(
+            {"detail": "Logout realizado com sucesso."}, status=status.HTTP_200_OK
+        )
