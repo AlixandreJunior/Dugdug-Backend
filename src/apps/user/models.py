625ac2f3590cd8f7bd0a45dbd53fc78451531924
Alixandria007 <juninhokrauts778@gmail.com>
2025-10-25 00:51:26 -0300

    Mais testes.

--- src/apps/user/models.py
+++ src/apps/user/models.py
@@ -12,7 +12,7 @@ class User(AbstractUser):
 
     email = models.EmailField(max_length=150, unique=True)
     cpf = models.CharField(max_length=14, unique=True, validators=[validate_cpf])
-    phone = models.CharField(max_length=15, unique=True, validators=[validate_phone])
+    phone = models.CharField(max_length=15, validators=[validate_phone])
     password = models.CharField(max_length=128, null=False)
 
     objects = UserManager["User"]()
